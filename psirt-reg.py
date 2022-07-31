57e633486e9fcbbd9357c1591ee788799441a89d
Aaron Davis <aaron.e.davis@gmail.com>
2022-07-31 16:12:51 -0600

    first commit

---
+++ psirt-reg.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""This script is the logic and brains for psirt-bot scriptions."""
+
+__author__ = "Aaron Davis"
+__version__ = "0.1.5"
+__copyright__ = "Copyright (c) 2022 Aaron Davis"
+__license__ = "MIT License"
+
+import configparser
+import logging
+from datetime import datetime, date, timedelta, timezone
+import os
+import sys
+import json
+from time import time
+import requests
+import certifi
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(r".\logs\debug.log"),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
+KEY = "CI"
+if os.getenv(KEY):
+    mongoaddr = "cluster0.jzvod.mongodb.net"
+    mongodb = "PSIRT"
+    mongocollect = "subscriptions"
+    mongouser = os.environ["mongouser"]
+    mongopw = os.environ["mongopw"]
+    webex_bearer = os.environ["webex_bearer"]
+else:
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    mongoaddr = config["MONGO"]["mongo_addr"]
+    mongodb = config["MONGO"]["mongo_db"]
+    mongocollect = config["MONGO"]["mongo_collect"]
+    mongouser = config["MONGO"]["user_name"]
+    mongopw = config["MONGO"]["password"]
+    webex_bearer = config["WEBEX"]["bearer"]
+
+MAX_MONGODB_DELAY = 500
+
+Mongo_Client = MongoClient(
+    f"mongodb+srv://{mongouser}:{mongopw}@{mongoaddr}/{mongodb}?retryWrites=true&w=majority",
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=MAX_MONGODB_DELAY,
+)
+
+db = Mongo_Client[mongodb]
+collection = db[mongocollect]
